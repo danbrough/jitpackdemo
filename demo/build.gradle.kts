37254ba4ecf865a48595ec563b6d2075417a82d2
Dan Brough <dan@danbrough.org>
2019-11-09 15:35:59 +1300

    update

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 dependencies {
 
 
-  implementation("com.github.danbrough.jitpackdemo:lib2:0.0.0")
+  implementation("com.github.danbrough.jitpackdemo:lib2:master-SNAPSHOT")
   implementation(project(":lib1"))
 
 
--- lib2/build.gradle.kts
+++ lib2/build.gradle.kts
@@ -1,69 +1,75 @@
-plugins {
-  id("com.android.library")
-  kotlin("android")
-  kotlin("kapt")
-  kotlin("android.extensions")
-  id("com.github.dcendents.android-maven") // IMPORTANT BIT
+
+
+
+apply {
+    plugin("com.github.dcendents.android-maven")
 }
 
 group = "com.github.danbrough.jitpackdemo" // IMPORTANT BIT
 
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("android.extensions")
+
+}
+
+
 
 android {
-  compileSdkVersion(ProjectVersions.SDK_VERSION)
-  defaultConfig {
-    minSdkVersion(ProjectVersions.MIN_SDK_VERSION)
-    targetSdkVersion(ProjectVersions.SDK_VERSION)
-    versionCode = ProjectVersions.VERSION_CODE
-    versionName = ProjectVersions.VERSION_NAME
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
+    compileSdkVersion(ProjectVersions.SDK_VERSION)
+    defaultConfig {
+        minSdkVersion(ProjectVersions.MIN_SDK_VERSION)
+        targetSdkVersion(ProjectVersions.SDK_VERSION)
+        versionCode = ProjectVersions.VERSION_CODE
+        versionName = ProjectVersions.VERSION_NAME
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
 
-  }
-
-  compileOptions {
-    sourceCompatibility = ProjectVersions.JAVA_VERSION
-    targetCompatibility = ProjectVersions.JAVA_VERSION
-  }
-
-  buildTypes {
-
-    getByName("release") {
-      isMinifyEnabled = true
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
     }
 
-  }
+    compileOptions {
+        sourceCompatibility = ProjectVersions.JAVA_VERSION
+        targetCompatibility = ProjectVersions.JAVA_VERSION
+    }
 
-  testOptions {
-    unitTests.isIncludeAndroidResources = true
-    unitTests.isReturnDefaultValues = true
-  }
+    buildTypes {
+
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+    }
+
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+        unitTests.isReturnDefaultValues = true
+    }
 
 }
 
 
 
 tasks.withType<Test> {
-  useJUnit()
-  testLogging {
-    events("standardOut", "started", "passed", "skipped", "failed")
-    showStandardStreams = true
-    /*  outputs.upToDateWhen {
-        false
-      }*/
-  }
+    useJUnit()
+    testLogging {
+        events("standardOut", "started", "passed", "skipped", "failed")
+        showStandardStreams = true
+        /*  outputs.upToDateWhen {
+            false
+          }*/
+    }
 }
 
 dependencies {
 
-  api(Libs.slf4j_api)
-  api(Libs.kotlin_stdlib_jdk8)
-
-
+    api(Libs.slf4j_api)
+    api(Libs.kotlin_stdlib_jdk8)
 
 
 }
