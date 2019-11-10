cc42ab78d4a8446451b5b13cc22bec33469244d5
Dan Brough <dan@danbrough.org>
2019-11-10 18:44:39 +1300

    1.0.0

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -38,8 +38,10 @@ android {
 dependencies {
 
 
-  implementation("com.github.danbrough.jitpackdemo:lib1:1.0.0")
-  implementation("com.github.danbrough.jitpackdemo:lib2:1.0.0")
+  implementation(project(":lib1"))
+  implementation(project(":lib2"))
+  //implementation("com.github.danbrough.jitpackdemo:lib1:1.0.0")
+  //implementation("com.github.danbrough.jitpackdemo:lib2:1.0.0")
 
 
 
