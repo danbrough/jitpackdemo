f0eeeb1b9b7838de829d08c8f2a1a1fa31f3e3d9
Dan Brough <dan@danbrough.org>
2019-11-11 09:41:12 +1300

    1.0.2

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -38,9 +38,13 @@ android {
 dependencies {
 
 
-
+	/*
   implementation("com.github.danbrough.jitpackdemo:lib1:1.0.0")
   implementation("com.github.danbrough.jitpackdemo:lib2:1.0.0")
+	*/
+ // implementation("com.github.danbrough.jitpackdemo:lib1:1.0.1")
+	implementation(project(":lib1"))
+	implementation(project(":lib2"))
 
 
 
--- lib2/src/main/java/danbroid/lib2/Library2.kt
+++ lib2/src/main/java/danbroid/lib2/Library2.kt
@@ -1,10 +1,9 @@
 package danbroid.lib2
 
-import java.util.*
 
 class Library2 {
   val message: String
-    get() = "Hello again from Library2 at ${Date()}"
+    get() = "Hello again from Library2 }"
 }
 
 private val log = org.slf4j.LoggerFactory.getLogger(Library2::class.java)
