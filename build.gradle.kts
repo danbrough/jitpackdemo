95e24490197d5e25c08c665547b626c0c3594ca1
Dan Brough <dan@danbrough.org>
2019-11-14 10:27:57 +1300

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,18 +7,13 @@ buildscript {
     classpath(Libs.com_android_tools_build_gradle)
     classpath(Libs.kotlin_gradle_plugin)
     classpath(Libs.navigation_safe_args_gradle_plugin)
-
-    // Libs.android_maven_gradle_plugin is "com.github.dcendents:android-maven-gradle-plugin:2.1"
-    //
-    // classpath(Libs.android_maven_gradle_plugin) //IMPORTANT BIT
-
-
   }
 
   repositories {
     google()
     jcenter()
   }
+
 }
 
 
@@ -49,7 +44,7 @@ allprojects {
 
 tasks {
   val dokka by getting(DokkaTask::class) {
-    outputFormat = "gfm"
+    outputFormat = "html" //or gfm 
     outputDirectory = "$rootDir/docs"
 
     subProjects = listOf("demo","lib1","lib2") //listOf("demo", "util", "permissions", "slf4j")
